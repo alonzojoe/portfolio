47bed2338bd2b9e4479027767ac5897c001d34a6
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-09-03 02:11:48 +0800

    Layout started Projects section

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ let menu = document.querySelector('#menu')
 let navbar = document.querySelector('.navbar')
 let header = document.querySelector('#header')
 const sections = document.querySelectorAll('section');
+const body = document.querySelector('body')
 
 
 menu.onclick = () => {
@@ -16,5 +17,6 @@ changeTheme.onclick = () => {
     header.classList.toggle('dark-mode')
     sections.forEach((s) => { s.classList.toggle('dark-mode') })
     changeTheme.classList.toggle('bxs-sun')
+    body.classList.toggle('dark-mode')
 }
 
