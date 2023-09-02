ed044b0d43e46035fcb024849e7fac41ab230cb9
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-09-02 20:32:07 +0800

    Added dark-mode toggle in navbar + function

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,18 @@
 
 let menu = document.querySelector('#menu')
 let navbar = document.querySelector('.navbar')
+let header = document.querySelector('#header')
+let sections = document.querySelector('section')
 
 menu.onclick = () => {
     navbar.classList.toggle('active')
 }
+
+let changeTheme = document.querySelector('#theme')
+
+changeTheme.onclick = () => {
+    header.classList.toggle('dark-mode')
+    sections.classList.toggle('dark-mode')
+    changeTheme.classList.toggle('bxs-sun')
+}
+
